1b674ac7873062af89184c327227a52bcb73f542
Thinking Song <123wuming_123@163.com>
2014-12-13 00:32:51 +0800

    change db config

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_study',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
