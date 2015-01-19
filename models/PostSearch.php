ee09d6984a8699a79d4f4e7024c014e1f999cbe4
Thinking Song <123wuming_123@163.com>
2015-01-19 14:24:36 +0800

    add post search

---
+++ models/PostSearch.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Post;
+
+/**
+ * PostSearch represents the model behind the search form about `app\models\Post`.
+ */
+class PostSearch extends Post
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['_id', 'name', 'content', 'createTime', 'updateTime'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Post::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere(['like', '_id', $this->_id])
+            ->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'content', $this->content])
+            ->andFilterWhere(['like', 'createTime', $this->createTime])
+            ->andFilterWhere(['like', 'updateTime', $this->updateTime]);
+
+        return $dataProvider;
+    }
+}
